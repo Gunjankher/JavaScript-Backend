1d9c0ca3ed398a256c1ff304ec3ee4082e2cc312
Gunjankher <gkhercode@gmail.com>
2024-03-07 10:32:33 +0530

    first backend app with express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi this is the Backend Repo");
+require('dotenv').config()
+console.log(process.env)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/home',(req,res)=>{
+res.send("this is a first express server")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
