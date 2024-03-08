eb3b9aa1887ac0caad83549065c14959c2551898
Gunjankher <gkhercode@gmail.com>
2024-03-08 11:17:04 +0530

    made front and backend

--- connect front end and Backend/Backend/script.js
+++ connect front end and Backend/Backend/script.js
@@ -3,3 +3,57 @@ import express from 'express'
 const app = express()
 
 
+app.get('/',(req,res)=>{
+res.send('server is ready')
+})
+
+
+app.get('/jokes', (req,res)=>{
+
+    const jokes  =[
+
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+},
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+},
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+},
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+},
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+},
+{
+    id: 1,
+    title : 'joke 1',
+    content : 'this is joke 1'
+}
+
+    ]
+    res.send(jokes)
+})
+
+
+
+const port  = process.env.PORT  || 4000
+
+
+
+app.listen (port ,()=>{
+console.log(`serve at the http://localhost:${port}`);
+
+})
