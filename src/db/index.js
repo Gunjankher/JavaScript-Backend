9b06fd17dc90d46b128b7c6719595abf2027a328
Gunjankher <gkhercode@gmail.com>
2024-03-15 10:43:37 +0530

    set up

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose  from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `/n Mongodb connected !! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+
+console.log("MONGODB CONNECTION ERROR", error);
+process.exit(1)
+
+  }
+};
+
+
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+import {DB_NAME} from './constants'
+require('dotenv').config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express"
+const app = express()
+(async()=>{
+try {
+    
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error)=>{
+        console.log("error", error);
+        throw error
+    })
+
+app.listen(process.env.PORT ,()=>{
+    console.log(`App is listening in Port ${process.env.PORT}`);
+
+})
+
+
+
+} catch (error) {
+    console.error('ERROR',error);
+    throw error
+}
+
+
+})()  */
+
+
+
+
+
+
+
+
