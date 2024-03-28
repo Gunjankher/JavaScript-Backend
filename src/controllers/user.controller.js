4a89a0bae5cba88b4ae7225aa0be422dd815b0a7
Gunjankher <gkhercode@gmail.com>
2024-03-28 10:57:29 +0530

    register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,26 @@
 import {asyncHandlar} from '../utils/asyncHandlar.js'
 
 const registerUser = asyncHandlar(async (req,res)=>{
-  res.status(200).json({
-    message : "finally done it"
-})
+  // res.status(200).json({
+  //   message : "finally done it"
+
+ // get details from user
+ // validation -not empty
+ // chack if user already exist : username and email 
+ // check for images and check for avatar
+ // upload them into cloudnary , avatar 
+ // create user object - create entry in db 
+ // remove password and refresh token field 
+ // check for the user creation 
+ // return response
+ 
+
+
+
+
 
 })
+
 
 
 export {registerUser}
