ed2eed32508bace1d47aea0a608976ddd90092b3
Gunjankher <gkhercode@gmail.com>
2024-03-29 09:20:57 +0530

    register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import {asyncHandlar} from '../utils/asyncHandlar.js'
+import {ApiError} from '../utils/ApiError.js'
 
 const registerUser = asyncHandlar(async (req,res)=>{
   // res.status(200).json({
   //   message : "finally done it"
 
- // get details from user
+ // get details from user from frontend
  // validation -not empty
  // chack if user already exist : username and email 
  // check for images and check for avatar
@@ -13,10 +14,14 @@ const registerUser = asyncHandlar(async (req,res)=>{
  // remove password and refresh token field 
  // check for the user creation 
  // return response
+
+// get data from front-end
+const {fullname,email,username,password} = req.body
+console.log("email", email);
+
+// validation
+
  
-
-
-
 
 
 })
