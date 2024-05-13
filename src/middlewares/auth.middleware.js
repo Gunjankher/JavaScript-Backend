ce8f35b6583e535199f8ce820fa8094646191f6a
Gunjankher <gkhercode@gmail.com>
2024-05-13 14:49:04 +0530

    middlewares

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,3 +1,4 @@
+import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { asyncHandlar } from "../utils/asyncHandlar";
 import { Jwt } from "jsonwebtoken";
@@ -9,17 +10,29 @@ import { Jwt } from "jsonwebtoken";
 
 
 
+
  export const varifyJWT = asyncHandlar(async (req,res, next)=>{
- const token = req.cookies?.acessToken || req.header
-("Authorization")?.replace("Bearer", "")
-
-
-if(!token){
-    throw new ApiError (401, "unauthorized token")
-}
-
-jwt.varify(token, process.env.ACCESS_TOKEN_SECRET)
-
-await User.findById(decodeToken?._id).select("-password")
+ try {
+    const token = req.cookies?.acessToken || req.header
+   ("Authorization")?.replace("Bearer", "")
+   
+   
+   if(!token){
+       throw new ApiError (401, "unauthorized token")
+   }
+   
+    const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+   
+    const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
+    
+   if(!user){
+       throw new ApiError(401 , "Invalid Acess Token")
+   }
+   
+    req.user = user; 
+    next()
+ } catch (error) {
+    throw new ApiError(401, error?.message || "Inavalid Access Token")
+ }
 
  })
